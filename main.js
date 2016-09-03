4db741332d0e50fa7d33bb38b64fe0d86eaf08c0
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2016-09-03 13:01:38 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var express = require('express');
+var app = express();
+
+
+
+function logreq(req, res, next){
+  1/0;
+  let now = new Date();
+  next();
+  console.log(`${now}:${req.hostname}:${req.originalUrl}:${res.statusCode}`);
+}
+
+app.use(logreq);
+app.use(express.static('public'));
+app.use('/vendor', express.static('bower_components'));
+
+var apiv1 = express();
+apiv1.get('/status', function(req,res){
+  res.status(200).json({status: 'ok', time: new Date()})
+});
+
+app.use('/api/v1', apiv1);
+
+app.listen(3000, function(){
+  console.log('App started: ' + app.get('env'));
+});
