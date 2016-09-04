291b4639bbff20ac2b94939507e810d6d5671698
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2016-09-04 13:52:21 +0200

    fix missing material deps in test

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,8 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-material/angular-material-mocks.js',
       'public/js/**.js',
       'test/**/*Spec.js'
     ],
