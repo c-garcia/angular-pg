c7f17c279b32caf9b9411c05f4afdff8c3c4524e
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2016-09-04 07:16:21 +0200

    add ui.router configuration and infra

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,35 @@
 'use strict';
 
 (function(angular){
-  angular.module("myApp",['ui.router', 'ngMaterial']);
+  angular.module("myApp",['ui.router', 'ngMaterial'])
+    .config(['$stateProvider', function($stateProvider, quoteService){
+      $stateProvider.state({
+        'name': 'list-quotes',
+        'url': '/list-quotes',
+        'template': '<h1>list quotes</h1>',
+        'controller': 'listQuotesCtrl as listQ'
+      });
+      $stateProvider.state({
+        'name': 'new-quote',
+        'url': '/new-quote',
+        'template': '<h1>new quote</h1>',
+        'controller': 'newQuoteCtrl as newQ'
+      })
+    }])
+    .controller('listQuotesCtrl', ['text', function(text){
+      this.text = text;
+      console.log('listQuotesCtrl started');
+    }])
+    .controller('newQuoteCtrl', [function(){
+      console.log('newQuoteCtrl started');
+    }])
+    .factory('quoteService', ['$q', function($q){
+      var listQuotes = function(){
+        return $q(function(resolve, reject){reject('to be implemented');});
+      };
+
+      return {
+        listQuotes: listQuotes
+      }
+    }]);
 })(angular);
