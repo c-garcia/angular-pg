e2f1009f3ea6c96e9422491319aeb5bc3e046280
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2016-09-04 11:18:22 +0200

    add karma grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    karma:{
+      unit: {
+        configFile: 'karma.conf.js',
+        autoWatch: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('default',['karma:unit']);
+
+}
