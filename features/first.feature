504bfc1def3ead9cf1abb25dc6de720778c67863
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2016-09-07 22:17:48 +0200

    add test cucumber feature

---
+++ features/first.feature
@@ -0,0 +1,7 @@
+Feature: first
+
+  Scenario: first scenario
+
+    Given I access the target page
+    When I click on the relevant link
+    Then an alert is shown
---
+++ features/step_definitions/first_steps.js
@@ -0,0 +1,22 @@
+'use strict';
+var chai = require('chai');
+var chaiP = require('chai-as-promised')
+chai.use(chaiP);
+var expect = chai.expect;
+
+module.exports = function(){
+  this.Given(/^I access the target page$/, function(){
+    return browser.get('http://localhost:3000/');
+  });
+
+  this.When(/^I click on the relevant link$/, function () {
+    browser.findElement(by.linkText('New quote')).click();
+    return browser.findElement(by.buttonText('Notify')).click();
+  });
+
+  this.Then(/^an alert is shown$/, function () {
+    var EC = protractor.ExpectedConditions;
+    var msg =  browser.driver.wait(EC.alertIsPresent(), 3000);
+    return expect(browser.switchTo().alert().getText()).to.eventually.equal('hola');
+  })
+}
